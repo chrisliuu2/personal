86c58784b7412501b5d3f9643fe57da4d84542d2
chrisliuu2 <117779132+chrisliuu2@users.noreply.github.com>
2022-11-30 12:54:25 -0800

    fowijefwf
    fwfwoeifjwfe

--- script.js
+++ script.js
@@ -28,18 +28,3 @@ function displayTime () {
 }
 
 setInterval(displayTime, 10);
-
-function vidPlay() {  
-  $("#video1").get(0).play();  
-};
-function vidPause() {
-	$("#video1").get(0).pause();
-};
-$(document).ready(function(){
-  $("#textToggler").click(function(){
-    $(".toggleText").toggle();
-  });
-});
-function toggleImage() {
-  $(".hiddenclickimg").toggle();
-};
